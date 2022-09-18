cfa22c359ff14284fe39ec3760be92fe5fbe8c72
SkYw0w <kulba2004@gmail.com>
2022-09-18 08:01:57 +0300

    Database

---
+++ dbo.Joke.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Joke] (
+    [Id]           INT            IDENTITY (1, 1) NOT NULL,
+    [JokeQuestion] NVARCHAR (MAX) NULL,
+    [JokeAnswer]   NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Joke] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
