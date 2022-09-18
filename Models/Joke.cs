a39e01910ddb7c24a9dc5a150378fb1ba9163c12
SkYw0w <kulba2004@gmail.com>
2022-09-18 07:37:43 +0300

    Add new model class

---
+++ Models/Joke.cs
@@ -0,0 +1,17 @@
+﻿namespace TastingWeb.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+
+    }
+}
